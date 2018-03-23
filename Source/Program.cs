34c0ee25c4bf6d86dbc270a9868108f5fdd80902
Avius <avius.ravis@gmail.com>
2018-03-23 16:34:53 +0100

    First iteration

---
+++ Source/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformUnixWinNewlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                String inputPath = args[0];
+                String outputPath = inputPath;
+                if (args.Length >= 2 && args[1] == "/c")
+                {
+                    string extension = "." + inputPath.Split('.').Last();
+                    outputPath = inputPath.Remove(inputPath.Length-extension.Length) + "_converted" + extension;
+                }
+
+                string[] lines;
+
+                using (StreamReader stream = new StreamReader(inputPath))
+                {
+                    lines = stream.ReadToEnd().Split('\n');
+                }
+                using (StreamWriter stream = new StreamWriter(outputPath, false))
+                {
+                    foreach (var line in lines)
+                        stream.Write(line + "\r\n");
+                }
+            }
+
+        }
+
+        static void HelpMessage()
+        {
+            Console.WriteLine("Use this program in this way:\n    TUWNl_converter.exe PATH_TO_FILE [/c]\n      use /c parameter if you want to create copy of file");
+        }
+    }
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TransformUnixWinNewlines")]
+[assembly: AssemblyDescription("Program for replacing unix new lines with windows new lines")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avius")]
+[assembly: AssemblyProduct("TransformUnixWinNewlines Converter")]
+[assembly: AssemblyCopyright("Freeware")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82e100e8-99a3-4d3b-8632-4582f21f842c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
